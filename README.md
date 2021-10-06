b3b2430c52a7597c5d35236ed68d5c6cede16c54
mestrada7@mail.sfsu.edu <44541773+melissa4444@users.noreply.github.com>
2021-10-05 21:14:58 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# OOD_TicTacToe
+ Program done as an exserise in Object Oriented Desing Course
---
+++ bin/main.rb
@@ -0,0 +1,83 @@
+require_relative '../lib/cell'
+require_relative '../lib/board'
+require_relative '../lib/player'
+require_relative '../lib/game_logic'
+
+system('clear')
+puts "Welcome to Ruby's Tic-Tac-Toe!"
+puts 'Enter First Player Name (X or O):'
+first_player_name = gets.chomp
+puts 'Enter Second Player Name:'
+second_player_name = gets.chomp
+
+game = GameLogic.new(first_player_name, second_player_name)
+system('clear')
+
+until game.game_end?
+  board = game.board
+  puts "Let's start"
+
+  puts '+---++---++---+'
+  (0...3).each do |x|
+    (0...3).each do |y|
+      position = x * 3 + y
+      if board.get_cell_value(position).valid?
+        print "| #{position + 1} |"
+      else
+        print "| #{board.get_cell_value(position).mark} |"
+      end
+    end
+    print "\r\n"
+    puts '+---++---++---+'
+  end
+  if game.player_win?(game.player_one)
+    puts "#{game.player_one.name} WINS!"
+    puts 'Game Over'
+    break
+  end
+  if game.player_win?(game.player_two)
+    puts "#{game.player_two.name} WINS!"
+    puts 'Game Over'
+    break
+  end
+
+  if game.turn.even?
+    puts "It's #{game.player_one.name}'s turn"
+    puts 'Please select an available cell from the board'
+    move = gets.chomp.to_i
+    until game.play_player?(game.player_one, move)
+      puts "Enter a valid value #{game.player_one.name}"
+      move = gets.chomp.to_i
+    end
+  else
+    puts "It's #{game.player_two.name} turn"
+    puts 'Please select an available cell from the board'
+    move = gets.chomp.to_i
+    until game.play_player?(game.player_two, move)
+      puts "Enter a valid value #{game.player_two.name}"
+      move = gets.chomp.to_i
+    end
+  end
+
+  game.next_turn
+  system('clear')
+end
+
+unless game.player_win?(game.player_one) || game.player_win?(game.player_two)
+
+  puts '+---++---++---+'
+  (0...3).each do |x|
+    (0...3).each do |y|
+      position = x * 3 + y
+      if board.get_cell_value(position).valid?
+        print "| #{position + 1} |"
+      else
+        print "| #{board.get_cell_value(position).mark} |"
+      end
+    end
+    print "\r\n"
+    puts '+---++---++---+'
+  end
+  puts 'Draw Game'
+  puts 'Game Over'
+end
